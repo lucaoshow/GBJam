7301b1281206131ced38e962cd8e852a2439bd19
lucaoshow <lucas.nunes@sou.inteli.edu.br>
2024-09-18 14:16:57 -0300

    add dialogue (and interaction) system!!!

--- src/GBJamGame/Assets/Scripts/Dialogue/DialogueManager.cs
+++ src/GBJamGame/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,18 +1,89 @@
-using System.Collections;
-using System.Collections.Generic;
+using Ink.Runtime;
+using TMPro;
 using UnityEngine;
 
-public class DialogueManager : MonoBehaviour
+namespace Root.Dialogues
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DialogueManager : MonoBehaviour
     {
-        
-    }
+        private GameObject dialogueBox;
+        private TextMeshProUGUI dialogueText;
+        private Story currentDialogue;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private string completeDialogueText = "";
+        private bool dialogueEnded = false;
+
+        private static DialogueManager instance;
+        public static DialogueManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = (new GameObject("DialogueManagerContainer")).AddComponent<DialogueManager>();
+                }
+                return instance;
+            }
+        }
+
+        private void Start()
+        {
+            DontDestroyOnLoad(this);
+            this.dialogueBox = Instantiate(Resources.Load<GameObject>("Prefabs/Dialogue/DialogueBox"));
+            this.dialogueText = this.dialogueBox.GetComponentInChildren<TextMeshProUGUI>();
+            this.dialogueBox.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (this.WritingText())
+            {
+                this.dialogueText.text += this.completeDialogueText[this.dialogueText.text.Length];
+            }
+        }
+
+        public void StartDialogue(TextAsset inkJson)
+        {
+            this.currentDialogue = new Story(inkJson.text);
+            this.dialogueEnded = false;
+            this.dialogueBox.SetActive(true);
+            this.ContinueDialogue();
+        }
+
+        public void ContinueDialogue()
+        {
+            if (this.WritingText())
+            {
+                Debug.Log(this.completeDialogueText.Length);
+                this.dialogueText.text = this.completeDialogueText;
+            }
+            else if (this.currentDialogue.canContinue)
+            {
+                this.dialogueText.text = "";
+                this.completeDialogueText = this.currentDialogue.Continue();
+            }
+            else
+            {
+                this.EndDialogue();
+            }
+        }
+
+        public bool EndedDialogue()
+        {
+            return this.dialogueEnded;
+        }
+
+        private bool WritingText()
+        {
+            return this.dialogueText.text.Length < this.completeDialogueText.Length;
+        }
+
+        private void EndDialogue()
+        {
+            this.dialogueBox.SetActive(false);
+            this.completeDialogueText = "";
+            this.dialogueText.text = "";
+            this.dialogueEnded = true;
+        }
     }
 }
---
+++ src/GBJamGame/Assets/Scripts/Interaction/Interectable.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Root.Interactions
+{
+    public abstract class Interactable : MonoBehaviour
+    {
+        [SerializeField] private GameObject interactionIcon;
+        protected bool stillInteractable = true;
+        
+        public virtual void Interact() { }
+
+        public virtual void Start()
+        {
+            this.interactionIcon.SetActive(false);
+        }
+
+        public virtual InteractionTypes GetInteractionType() 
+        {
+            return InteractionTypes.Dialogue;
+        }
+
+        public virtual bool CanInteract()
+        {
+            return this.stillInteractable;
+        }
+
+        public void InteractionIconVisible(bool visible)
+        {
+            if (this.stillInteractable)
+            {
+                this.interactionIcon.SetActive(visible);
+            }
+        }
+        
+        protected void DisableInteraction()
+        {
+            this.InteractionIconVisible(false);
+            this.stillInteractable = false;
+        }
+    }       
+}
