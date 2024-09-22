6c7188572f901efd9bb2907fe24848efb81d7bed
lucaoshow <lucas.nunes@sou.inteli.edu.br>
2024-09-22 09:36:25 -0300

    importing art

--- src/GBJamGame/Assets/Scripts/Monster/Monster.cs
+++ src/GBJamGame/Assets/Scripts/Monster/Monster.cs
@@ -6,6 +6,7 @@ namespace Root.Monster
     public class Monster : MonoBehaviour
     {
         [SerializeField] private Transform target;
+        [SerializeField] private float moveSpeed;
         private NavMeshAgent agent;
 
         void Start()
@@ -18,6 +19,28 @@ namespace Root.Monster
         void Update()
         {
             this.agent.SetDestination(this.target.position);
+            this.agent.velocity = this.GetDirectionFromVelocity(this.agent.desiredVelocity) * this.moveSpeed;
         }
+
+        private Vector3 GetDirectionFromVelocity(Vector3 velocity)
+        {
+            Vector3[] directions = { Vector3.down, Vector3.up, Vector3.left, Vector3.right };
+            float smallest = 1;
+            Vector3 finalDir = Vector3.zero;
+
+            foreach (Vector3 dir in directions)
+            {
+                float current = Vector3.Dot(velocity, dir);
+                if (current < smallest)
+                {
+                    smallest = current;
+                    finalDir = dir;
+                }
+            }
+
+            return -finalDir;
+        }
+
+
     }
 }
